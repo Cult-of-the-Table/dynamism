9faa7300f0d15b3d286b725594292873fe3ecfa3
miloa <crabapplefarms@icloud.com>
2026-03-15 17:31:55 -0500

    implemented websearch and default test

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 use tokio;
 pub mod db;
 pub mod embed;
+pub mod websearch;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     Ok(())
---
+++ src/websearch.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+use websearch::{
+    multi_provider::{
+        MultiProviderConfig, MultiProviderSearch, MultiProviderStrategy, SearchOptionsMulti,
+    },
+    providers::DuckDuckGoProvider,
+};
+
+async fn search(query: &str) -> Result<()> {
+    let provider = DuckDuckGoProvider::new();
+    let strategy = MultiProviderStrategy::Aggregate;
+    let schema = MultiProviderConfig::new(strategy);
+    let schema = schema.add_provider(Box::new(provider));
+    let mut multi = MultiProviderSearch::new(schema);
+    let search = SearchOptionsMulti {
+        query: query.to_string(),
+        ..Default::default()
+    };
+    let results = multi.search(&search).await;
+    results.iter().for_each(|s| {
+        s.iter().for_each(|v| {
+            println! {"{}",v.title};
+        })
+    });
+    Ok(())
+}
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn init() -> Result<()> {
+        let query = "rust";
+        let _ = search(query).await;
+        Ok(())
+    }
+}
