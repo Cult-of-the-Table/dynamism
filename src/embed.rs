1ddaa3d685963cdf928c3f116cd02fca62c8da09
miloa <crabapplefarms@icloud.com>
2026-03-14 20:21:23 -0500

    added embedding test for fastembed

--- src/embed.rs
+++ src/embed.rs
@@ -1,6 +1,7 @@
 use fastembed::{Embedding, EmbeddingModel, Error, InitOptions, TextEmbedding};
 
-pub fn embd(data: &[String]) -> Result<Vec<Embedding>, Error> {
+pub fn embd(data: Vec<&str>) -> Result<Vec<Embedding>, Error> {
+    let data = data.iter().map(|s| s.to_string()).collect::<Vec<String>>();
     let mut model = TextEmbedding::try_new(
         InitOptions::new(EmbeddingModel::NomicEmbedTextV15).with_show_download_progress(true),
     )?;
@@ -9,4 +10,13 @@ pub fn embd(data: &[String]) -> Result<Vec<Embedding>, Error> {
         s
     });
     embeddings
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn init() {
+        let v = vec!["test", "test2"];
+        let _ = embd(v);
+    }
 }
