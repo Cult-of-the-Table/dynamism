08c1b1707071b579ceed0aa53ddaa873780b2bac
miloa <crabapplefarms@icloud.com>
2026-04-08 15:08:38 -0500

    multi-webapge integration test rewrite

--- tests/integration.rs
+++ tests/integration.rs
@@ -19,25 +19,28 @@ async fn init_pipe() -> Result<()> {
         let u = u.to_string();
         set.spawn(async move { parse((s, u)).await });
     }
-    let mut parse = (String::new(), String::new());
+    let mut parse: Vec<(String, String)> = Vec::new();
     while let Some(res) = set.join_next().await {
-        parse = res??;
+        parse.push(res??);
     }
-    let task = EmbeddingTask {
-        source_text: parse.0,
-        url: parse.1,
-    };
+
+    let task = parse.iter().map(|s| EmbeddingTask {
+        source_text: s.0.to_string(),
+        url: s.1.to_string(),
+    });
 
     let (tel, tel_handle) = dynamism::telemetry::spawn();
     let (tx, rx, seg_handle) = dynamism::segmentation::worker::spawn(tel.clone());
-    tx.send(task).await.unwrap();
-    drop(tx);
+    for t in task {
+        tx.send(t).await.unwrap();
+    }
     let dir = tempdir()?;
     let db_handle = spawn(
         rx,
         dir.path().to_str().unwrap().to_string(),
         "test".to_string(),
     );
+    drop(tx);
     drop(tel);
     db_handle.await.unwrap();
     seg_handle.await.unwrap();
