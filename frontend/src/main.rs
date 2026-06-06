0e403f31a31706e3c46233c836dc6b71e4ec522a
miloa <crabapplefarms@icloud.com>
2026-06-06 17:13:51 -0500

    it works

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,14 +1,29 @@
-use iced::Theme;
-pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    iced::daemon(u64::default(), Dynamism::update, Dynamism::view)
-        .theme(Theme::Dark);
+use iced::{Element, Task, Theme, widget::container};
+
+pub fn main() -> iced::Result {
+    iced::application(Dynamism::new, Dynamism::update, Dynamism::view)
+        .theme(Theme::Dark)
+        .run()?;
+
     Ok(())
 }
-struct Dynamism {}
+#[derive(Debug, Clone)]
 enum Message {
     Welcome,
 }
+
+struct Dynamism {}
+
 impl Dynamism {
-    fn update(&mut self, message: Message) {}
-    fn view(value: u64) {}
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _message: Message) -> Task<Message> {
+        Task::none()
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        container("test").style(container::rounded_box).into()
+    }
 }
